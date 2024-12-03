371d2d29d900665edefe344296d886ae4e34b763
Eric Wang <137523478+erichwang2005@users.noreply.github.com>
2024-12-03 18:29:29 -0500

    Update hash_map.h
    Added all definitions needed for this class

--- hash_map.h
+++ hash_map.h
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "movie.cpp"
+#include <functional>
 
+using namespace std;
+
+class popPickTable
+{
+    public:
+        popPickTable(long size = 10, double threshold = 0.7);
+        void insert(const string& key, const Movie& value);
+        void search(const string& key) const;
+        void printTable() const;
+    private:
+        struct keyValuePair
+        {
+            string key;
+            Movie value;
+            keyValuePair();
+            keyValuePair(string k, Movie v);
+        };
+
+        vector<keyValuePair> table;
+        vector<bool> occupied;
+        long numElements;
+        long capacity;
+        double loadThreshold;
+
+        void rehash();
+        int hashFunction(const string& key) const;
+
+};
