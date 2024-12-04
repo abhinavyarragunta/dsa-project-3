8a0d094fcf930f726ca24a4e7f93890a98bf7a74
abhiyarr <abhinavyarragunta05@gmail.com>
2024-12-03 22:04:51 -0500

    Updated Movie object: genres vector and add methods.

--- movie.cpp
+++ movie.cpp
@@ -5,26 +5,22 @@ using namespace std;
 class Movie {
 private:
     string name;
-    vector<string> genre;
     double rating;
     double length;
     int year;
+    vector<string> genres;
     vector<string> cast;
     vector<string> tags;
 
 public:
-    Movie(string n, string g, double r = 0, double l = 0, int y = 0000) {
+    Movie(string n, double r = 0, double l = 0, int y = 0000) {
         name = n;
-        genre = g;
-        year = y;
         rating = r < 0 ? 0 : r;
         length = l < 0 ? 0 : l;
+        year = y;
     }
-    string getName() {return name;}
-    vector<string> getGenre() {return genre;}
-    double getRating() {return rating;}
-    double getLength() {return length;}
-    int getYear() {return year;}
-    vector<string> getCast() {return cast;}
-    vector<string>getTags() {return tags;}
+
+    void addGenre(string genre) {genres.push_back(genre);}
+    void addActor(string actorName) {cast.push_back(actorName);}
+    void addTag(string tag) {tags.push_back(tag);}
 };
