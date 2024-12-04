58492492ef6bff9cd0d0618f97d68e929e963f07
Tristan <147774910+TristanRatchev@users.noreply.github.com>
2024-12-04 18:38:01 -0500

    QOL cout updates
    deleted unnecessary new lines in UI

--- main.cpp
+++ main.cpp
@@ -57,7 +57,7 @@ int main() {
                      "            |_|  \n""";
 
         std::cout << "Please choose which data structure you would like to use: (1) hash map (2) graph\n";
-        std::cout << "Choose an option (1, 2)\n\n";
+        std::cout << "Choose an option (1, 2)";
 
         string dataStructureChoice;
         cin >> dataStructureChoice;
