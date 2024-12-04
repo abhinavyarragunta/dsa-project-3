5753b2374b76b017d6d55797e2effb9b4c53970a
Eric Wang <137523478+erichwang2005@users.noreply.github.com>
2024-12-04 03:17:42 -0500

    Update hash_recommender.cpp
    Added a main for testing. Redid algorithm for recommedations in order to align with original vision of the project.

--- hash_recommender.cpp
+++ hash_recommender.cpp
@@ -4,40 +4,22 @@
 #include "hash_map.h"
 #include <algorithm>
 #include <iterator>
+#include <unordered_set>
 
 using namespace std;
 
-vector<string> hashRecommender(popPickTable& movieTable, const string& title)
+vector<string> hashRecommender(popPickTable& movieTable, const unordered_set<string>& sharedGenres)
 {
-    //if title exists within hashtable
-        //then search for its genres
-        //then find movie with similar genres as the ones in title
-        // return a vector of movies that have similar genres
-
     vector<string> recommendations;
-    string foundTitle = movieTable.searchTitle(title);
-    if(foundTitle == "Not Found")
-    {
-        cout << "Movie not found!" << '\n';
-        return recommendations;
-    }
-
-    vector<string> genres = movieTable.searchGenres(title);
-    if(genres.empty())
-    {
-        cout << "No genres found for " << title << '\n';
-        return recommendations;
-    }
-
     for(int i = 0; i < movieTable.size(); i++)
     {
         if(movieTable.occupied[i])
         {
-            vector<string> currentGenres = movieTable.table[i].value;
+            const vector<string>& movieGenres = movieTable.table[i].value;
 
-            for(const string& genre : genres)
+            for(const string& genre : movieGenres)
             {
-                if(std::find(currentGenres.begin(), currentGenres.end(), genre) != currentGenres.end())
+                if(sharedGenres.find(genre) != sharedGenres.end())
                 {
                     recommendations.push_back(movieTable.table[i].key);
                     break;
@@ -45,6 +27,59 @@ vector<string> hashRecommender(popPickTable& movieTable, const string& title)
             }
         }
     }
-
     return recommendations;
+}
+
+int main() {
+    // Create and populate the hash table
+    popPickTable movieTable(10);
+    movieTable.insert("Jaws", {"Horror", "Shark"});
+    movieTable.insert("Wicked", {"Musical"});
+    movieTable.insert("Se7en", {"Mystery", "Thriller"});
+    movieTable.insert("Finding Nemo", {"Adventure", "Family"});
+    movieTable.insert("Inception", {"Thriller", "Sci-Fi"});
+
+    cout << "User 1: Enter your favorite genres (type 'q' to finish):" << endl;
+    unordered_set<string> user1Genres;
+    string genre;
+    while (true) {
+        cin >> genre;
+        if (genre == "q") break;
+        user1Genres.insert(genre);
+    }
+
+    cout << "User 2: Enter your favorite genres (type 'q' to finish):" << endl;
+    unordered_set<string> user2Genres;
+    while (true) {
+        cin >> genre;
+        if (genre == "q") break;
+        user2Genres.insert(genre);
+    }
+
+    // Find shared genres between User 1 and User 2
+    unordered_set<string> sharedGenres;
+    for (const string& genre : user1Genres) {
+        if (user2Genres.find(genre) != user2Genres.end()) {
+            sharedGenres.insert(genre);
+        }
+    }
+
+    if (sharedGenres.empty()) {
+        cout << "No shared genres found. No recommendations available." << endl;
+        return 0;
+    }
+
+    // Get movie recommendations
+    vector<string> recommendations = hashRecommender(movieTable, sharedGenres);
+
+    if (recommendations.empty()) {
+        cout << "No movies found for shared genres." << endl;
+    } else {
+        cout << "Recommended movies based on shared genres:" << endl;
+        for (const string& movie : recommendations) {
+            cout << "- " << movie << endl;
+        }
+    }
+
+    return 0;
 }
