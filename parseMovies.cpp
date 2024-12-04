6b5e075c2cbbc3983cc7d70c8433ca5a4404e1e9
Tristan <147774910+TristanRatchev@users.noreply.github.com>
2024-12-04 03:14:49 -0500

    Create parseMovies.cpp
    csv movie organizing function foundation

---
+++ parseMovies.cpp
@@ -0,0 +1,8 @@
+#include "parseMovies.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+std::unordered_map<std::string, std::vector<std::string>> parseMovies(const std::string& csvFilePath) {
+    return movieMap;
+}
